36ef47797512e054c3949686765ac1cc12d4ed32
LZL <403288582@qq.com.com>
2019-06-14 09:03:25 +0800

    ll

--- js/chess.js
+++ js/chess.js
@@ -1,14 +1,16 @@
 $(function () {
+
+    let black = {value: "黑子"}, white = {value: "白子"};
+    let blank = {};
+    let ai = true;
     let box = $(".box");
     let flag = false;
     for (let i = 0; i < 15; i++) {
         for (let j = 0; j < 15; j++) {
             $("<div>").addClass("chess").appendTo(box).attr('id', i + '_' + j);
+            blank[i + "_" + j] = true;
         }
     }
-
-    let black = {value:"黑子"}, white = {value:"白子"};
-
 
     $(box).on("click", ".chess", function () {
         let _this = $(this);
@@ -19,16 +21,59 @@ $(function () {
             if (flag) {
                 $(this).addClass("black");
                 black[coords] = true;
+                delete blank[coords];
                 flag = !flag;
                 judge(black, coords);
+                if (judge(black, coords) >= 5) {
+                    box.off("click");
+                    alert("黑子赢");
+
+                }
             } else {
                 $(this).addClass("white");
                 white[coords] = true;
+                delete blank[coords];
                 flag = !flag;
                 judge(white, coords);
+                if (judge(white, coords) >= 5) {
+                    box.off("click");
+                    alert("白子赢");
+                }
+            }
+            if (ai) {
+                let pos = aifn();
+                console.log(pos);
+                black[pos] = true;
+                delete blank[pos];
+                $("#" + pos).addClass("black");
+                if (judge(black, pos) >= 5) {
+                    alert("黑子赢");
+                    box.off("click");
+                }
+                flag = !flag;
             }
         }
     });
+
+    function aifn() {
+        let blackScore = 0, whiteScore = 0;
+        let pos1 = "", pos2 = "";
+        for (let i in blank) {
+            let score = judge(black, i);
+            if (score >= blackScore) {
+                blackScore = score;
+                pos1 = i;
+            }
+        }
+        for (let i in blank) {
+            let score = judge(white, i);
+            if (score >= whiteScore) {
+                whiteScore = score;
+                pos2 = i;
+            }
+        }
+        return blackScore >= whiteScore ? pos1 : pos2;
+    }
 
     function judge(obj, coords) {
         let [i, j] = coords.split("_");
@@ -52,31 +97,31 @@ $(function () {
         while (obj[--x + "_" + y]) {
             n2++;
         }
-        x =i * 1;
+        x = i * 1;
         while (obj[++x + "_" + (++y)]) {
             n3++;
         }
-        x=i*1;
-        y=j*1;
+        x = i * 1;
+        y = j * 1;
         while (obj[--x + "_" + (--y)]) {
             n3++;
         }
-        x=i*1;
-        y=j*1;
+        x = i * 1;
+        y = j * 1;
         while (obj[--x + "_" + (++y)]) {
             n4++;
         }
-        x=i*1;
-        y=j*1;
+        x = i * 1;
+        y = j * 1;
         while (obj[++x + "_" + (--y)]) {
             n4++;
         }
-        if(Math.max(n1,n2,n3,n4)>=5){
-            $(box).off("click");
-            alert("大吉大利，恭喜"+obj.value+"恰鸡！")
-        }
+
+            return Math.max(n1, n2, n3, n4);
+
     }
-    $(".refresh").on("click",function () {
+
+    $(".refresh").on("click", function () {
         location.reload(true);
     })
 });
